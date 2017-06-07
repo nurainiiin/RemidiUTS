73307b5b36fae92534ed91d0bd920ccdc3116536
nurainiiin <iinnuraini360@gmail.com>
2017-06-07 22:25:34 +0700

    done

---
+++ app/src/main/java/xir5n13/remidiuts/telkomschid/utsremidi/MainActivity.java
@@ -0,0 +1,46 @@
+package xir5n13.remidiuts.telkomschid.utsremidi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Navigation");
+
+        Button bt1 = (Button) findViewById(R.id.button1);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SatuActivity.class));
+            }
+        });
+        Button bt2 = (Button) findViewById(R.id.button2);
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DuaActivity.class));
+            }
+        });
+        Button bt3 = (Button) findViewById(R.id.button3);
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TigaActivity.class));
+            }
+        });
+        Button bt4 = (Button) findViewById(R.id.button4);
+        bt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EmpatActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/test/java/xir5n13/remidiuts/telkomschid/utsremidi/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package xir5n13.remidiuts.telkomschid.utsremidi;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
